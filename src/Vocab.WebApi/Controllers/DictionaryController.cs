80a185852411a7053cefaaf888da802609431a04
Aleksandr <sanya.romaniuta@gmail.com>
2024-04-11 23:22:35 +0300

    Добавлены контроллеры без реализации

---
+++ src/Vocab.WebApi/Controllers/DictionaryController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Vocab.WebApi.Controllers
+{
+    [ApiController, Route("dictionaries")]
+    public class DictionaryController : ControllerBase
+    {
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return StatusCode(((int)HttpStatusCode.NotImplemented));
+        }
+
+        [HttpDelete, Route("{id}")]
+        public IActionResult Delete(ulong id)
+        {
+            return StatusCode(((int)HttpStatusCode.NotImplemented));
+        }
+
+        [HttpPatch]
+        public IActionResult Patch()
+        {
+            return StatusCode(((int)HttpStatusCode.NotImplemented));
+        }
+    }
+}
---
+++ src/Vocab.WebApi/Controllers/StatementPairController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using Vocab.Application.Abstractions.Services;
+
+namespace Vocab.WebApi.Controllers
+{
+    [ApiController, Route("statement-pairs")]
+    public class StatementPairController: ControllerBase
+    {
+        [HttpPut]
+        public  IActionResult Put()
+        {
+            return StatusCode(((int)HttpStatusCode.NotImplemented));
+        }
+
+        [HttpDelete, Route("{id}")]
+        public  IActionResult Delete(ulong id)
+        {
+            return StatusCode(((int)HttpStatusCode.NotImplemented));
+        }
+
+        [HttpPatch]
+        public  IActionResult Patch()
+        {
+            return StatusCode(((int)HttpStatusCode.NotImplemented));
+        }
+
+
+        [HttpPatch, Route("{id}/rating/increase")]
+        public  IActionResult IncreaseRating(ulong id)
+        {
+            return StatusCode(((int)HttpStatusCode.NotImplemented));
+        }
+
+        [HttpPatch, Route("{id}/rating/decrease")]
+        public  IActionResult DecreaseRating(ulong id)
+        {
+            return StatusCode(((int)HttpStatusCode.NotImplemented));
+        }
+
+        [HttpGet, Route("{id}/rating")]
+        public  IActionResult GetRating(ulong id)
+        {
+            return StatusCode(((int)HttpStatusCode.NotImplemented));
+        }
+    }
+}
