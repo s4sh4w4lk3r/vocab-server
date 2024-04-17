eb3519afbe842722966c60b1b6d15c66f55aef90
Aleksandr <sanya.romaniuta@gmail.com>
2024-04-17 19:02:16 +0300

    Написаны валидаторы для словарей и выражений

---
+++ src/Vocab.Application/Validators/StatementDictionaryValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Vocab.Core.Entities;
+
+namespace Vocab.Application.Validators
+{
+    public class StatementDictionaryValidator : AbstractValidator<StatementDictionary>
+    {
+        public StatementDictionaryValidator(bool isIdValidationRequired = true)
+        {
+            _ = isIdValidationRequired ? RuleFor(sd => sd.Id).NotEmpty() : null;
+
+            RuleFor(sd => sd.Name).NotEmpty();
+            RuleFor(sd => sd.OwnerId).NotEmpty();
+            RuleFor(sd => sd.LastModified).NotEmpty();
+
+            RuleFor(sd => sd.StatementPairs).Null();
+        }
+    }
+}
---
+++ src/Vocab.Application/Validators/StatementPairValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Vocab.Core.Entities;
+
+namespace Vocab.Application.Validators
+{
+    public class StatementPairValidator : AbstractValidator<StatementPair>
+    {
+        public StatementPairValidator(bool isIdValidationRequired = true)
+        {
+            _ = isIdValidationRequired ? RuleFor(sp => sp.Id).NotEmpty() : null;
+
+            RuleFor(sp => sp.RelatedDictionaryId).NotEmpty();
+            RuleFor(sp => sp.Source).NotEmpty();
+            RuleFor(sp => sp.Target).NotEmpty();
+            RuleFor(sp => sp.StatementCategory).IsInEnum();
+            RuleFor(sp => sp.LastModified).NotEmpty();
+            RuleFor(sp => sp.GuessingLevel).GreaterThanOrEqualTo(StatementPair.MIN_GUESSING_LEVEL).LessThanOrEqualTo(StatementPair.MAX_GUESSING_LEVEL);
+
+            RuleFor(sp => sp.StatementsDictionary).Null();
+        }
+    }
+}
