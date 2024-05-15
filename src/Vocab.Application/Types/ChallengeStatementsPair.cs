bdf94cc80e9b317fae46b9c25d25f503b118c9c0
Aleksandr <sanya.romaniuta@gmail.com>
2024-05-15 23:26:17 +0300

    ChallengeStatementsPair сделан в виде record класса

--- src/Vocab.Application/Types/ChallengeStatementsPair.cs
+++ src/Vocab.Application/Types/ChallengeStatementsPair.cs
@@ -1,22 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
+﻿
 namespace Vocab.Application.Types
 {
-    public class ChallengeStatementsPair
-    {
-
-        public required long StatementsPairId { get; init; }
-        public required string Source { get; init; }
-        public required string Target1 { get; init; }
-        public required string Target2 { get; init; }
-
-        [SetsRequiredMembers]
-        public ChallengeStatementsPair(long statementsPairId, string source, string target1, string target2)
-        {
-            StatementsPairId = statementsPairId;
-            Source = source;
-            Target1 = target1;
-            Target2 = target2;
-        }
-    }
+    public record class ChallengeStatementsPair(long StatementsPairId, string Source, string Target1, string Target2);
 }
