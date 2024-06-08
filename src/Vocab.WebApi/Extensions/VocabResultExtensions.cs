efb63d74e04828f0e22b7bda7acceb89dc1ec0fa
Aleksandr <sanya.romaniuta@gmail.com>
2024-06-09 00:52:38 +0300

    Изменен VocabResult для информативности

--- src/Vocab.WebApi/Extensions/VocabResultExtensions.cs
+++ src/Vocab.WebApi/Extensions/VocabResultExtensions.cs
@@ -7,18 +7,26 @@ namespace Vocab.WebApi.Extensions
     internal static class VocabResultExtensions
     {
         public static IActionResult ToActionResult(this ResultVocab resultVocab)
+            => resultVocab.Success ? HandleOkResult(resultVocab) : HandleFailResult(resultVocab);
+
+        private static IActionResult HandleOkResult(ResultVocab resultVocab)
         {
-            if (resultVocab.Success is false && resultVocab.Description == ResultMessages.NotFound)
+            if (resultVocab.Description == ResultMessages.Added)
+            {
+                return new CreatedResult();
+            }
+
+            return new OkObjectResult(resultVocab);
+        }
+
+        private static IActionResult HandleFailResult(ResultVocab resultVocab)
+        {
+            if (resultVocab.Description == ResultMessages.NotFound)
             {
                 return new NotFoundObjectResult(resultVocab);
             }
 
-            if (resultVocab.Success is false)
-            {
-                return new BadRequestObjectResult(resultVocab);
-            }
-
-            return new OkObjectResult(resultVocab);
+            return new BadRequestObjectResult(resultVocab);
         }
 
     }
