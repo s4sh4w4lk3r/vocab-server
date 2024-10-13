7ab4fc24c2fa038aa79a3b088e3b71cf4321a7cc
Aleksandr <sanya.romaniuta@gmail.com>
2024-10-13 17:02:25 +0300

    Обновлены версии зависимотей

--- src/Vocab.WebApi/Extensions/ServiceCollectionExtensions.cs
+++ src/Vocab.WebApi/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,5 @@
 ﻿using Hangfire;
 using Hangfire.PostgreSql;
-using Keycloak.AuthServices.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using System.Reflection;
